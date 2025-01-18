f1978c7c703265e889aa4d40098e9cf7d3465c51
Vlad <serdyuk2slava@gmail.com>
2025-01-18 18:12:47 +0300

    just commit

--- publisher.py
+++ publisher.py
@@ -5,9 +5,10 @@ import rospy
 from std_msgs.msg import String
 
 pub = rospy.Publisher('hello', String, queue_size=10)
-rospy.init_node('hello_topic_publisher')
-r = rospy.Rate(10) # 10hz
+rospy.init_node('base_topic_publisher')
 
 while not rospy.is_shutdown():
-    pub.publish("Hello World")
-    r.sleep()
+    pub.publish("a")
+    rospy.sleep(1)
+    pub.publish("b")
+    rospy.sleep(1)
--- subscriber.py
+++ subscriber.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
+
 import rospy
 from std_msgs.msg import String
 
 def callback(data):
     rospy.loginfo("I heard %s",data.data)
+    os.system(f'echo {data.data} > /dev/ttyACM0')
 
 def subscriber():
-    rospy.init_node('hello_topic_subscriber')
+    rospy.init_node('base_topic_publisher')
     rospy.Subscriber("hello", String, callback)
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()

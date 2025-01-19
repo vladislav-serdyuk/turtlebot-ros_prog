5f67617a01e2c739392515bba23f1441226edbcd
Vlad <serdyuk2slava@gmail.com>
2025-01-19 20:04:00 +0300

    just commit

--- publisher.py
+++ publisher.py
@@ -3,14 +3,30 @@
 
 import rospy
 from std_msgs.msg import Int32
+from pynput.keyboard import Key, Listener
 
-pub = rospy.Publisher('speed_L', Int32, queue_size=10)
 rospy.init_node('pc')
+left_speed = rospy.Publisher('speed_L', Int32, queue_size=10)
+right_speed = rospy.Publisher('speed_L', Int32, queue_size=10)
 
+def show(key):
+    if key == 'w':
+        left_speed.publish(speed)
+        right_speed.publish(speed)
+    elif key == 'a':
+        left_speed.publish(-speed)
+        right_speed.publish(speed)
+    elif key == 'd':
+        left_speed.publish(speed)
+        right_speed.publish(-speed)
+    elif key == 's':
+        left_speed.publish(-speed)
+        right_speed.publish(-speed)
+    else:
+        left_speed.publish(0)
+        right_speed.publish(0)
+
+speed = 50
 while not rospy.is_shutdown():
-    for speed in range(40, -40, -5):
-        pub.publish(speed)
-        rospy.sleep(0.1)
-    for speed in range(-40, 40, 5):
-        pub.publish(speed)
-        rospy.sleep(0.1)
+    with Listener(on_press=show) as listener:
+        listener.join()
